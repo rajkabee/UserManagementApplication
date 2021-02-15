c2e81d90e7dff8e1b50f8623dcde711f0eca8850
rajkabee <rajkabee@gmail.com>
2021-02-15 22:43:09 +0545

    third commit

--- UserManagementApplication/src/main/java/com/hospital/userManagement/UserManagementApplication.java
+++ UserManagementApplication/src/main/java/com/hospital/userManagement/UserManagementApplication.java
@@ -2,8 +2,10 @@ package com.hospital.userManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient//not mandatory
 public class UserManagementApplication {
 
 	public static void main(String[] args) {
---
+++ client/src/test/java/com/hospital/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
